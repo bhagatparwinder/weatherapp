0470d5437905b7a8425270ed262541218bcefa69
Parwinder Bhagat <singhparwinder@me.com>
2015-01-31 17:43:17 -0600

    API Integration & Update View

--- js/app.js
+++ js/app.js
@@ -1,10 +1,43 @@
-var Todo = Backbone.Model.extend({});
-
-// or with some arbitrary data:
-var Initiate = new Todo({
-    title: 'Check the attributes of both model instances in the console.',
-    completed: true
+var Weather = Backbone.Model.extend({
+    url: 'http://api.openweathermap.org/data/2.5/forecast/daily'
 });
 
-// Following logs: {"title":"Check the attributes of both model instances in the console.","completed":true}
-console.log(JSON.stringify(Initiate));
+var WeatherView = Backbone.View.extend({
+    el: '.filter-holder',
+    initialize: function(options) {
+        this.model = new Weather;
+        this.listenTo(this.model, "change", this.render);
+    },
+    events: {
+        "click .fetch-weather-trigger": "fetchWeather",
+        "click .fetch-detailed-trigger": "fetchDetailedWeather"
+    },
+    render: function() {
+        _.templateSettings.variable = "rc";
+        var template = _.template(
+            $(".weatherTemplate").html()
+        );
+        var weatherData = this.model.get("list");
+        $(".center-wrapper").empty().append(
+            template(weatherData)
+        );
+    },
+    fetchWeather: function(e) {
+        var city = $('.user-city-input').val();
+        this.model.fetch({
+            traditional: true,
+            data: $.param({
+                q: city,
+                units: 'imperial',
+                cnt: 10
+            }),
+            success: function() {},
+            error: function(err) {}
+        });
+    },
+    fetchDetailedWeather: function(e) {
+        $('.detailed').show()
+    }
+});
+
+var view = new WeatherView();
