8ea2c8cdec9aa99a2878d3d092344328815d13ca
Parwinder Bhagat <singhparwinder@me.com>
2015-02-01 20:52:00 -0600

    Finishing Acceptance Criteria

--- js/app.js
+++ js/app.js
@@ -3,14 +3,13 @@ var Weather = Backbone.Model.extend({
 });
 
 var WeatherView = Backbone.View.extend({
-    el: '.filter-holder',
+    el: '.query-holder',
     initialize: function(options) {
         this.model = new Weather;
         this.listenTo(this.model, "change", this.render);
     },
     events: {
         "click .fetch-weather-trigger": "fetchWeather",
-        "click .fetch-detailed-trigger": "fetchDetailedWeather"
     },
     render: function() {
         _.templateSettings.variable = "rc";
@@ -21,23 +20,46 @@ var WeatherView = Backbone.View.extend({
         $(".center-wrapper").empty().append(
             template(weatherData)
         );
+        $('.fetch-detailed-trigger-trigger').removeClass('active');
+        $('.fetch-summary-trigger').addClass('active');
     },
     fetchWeather: function(e) {
         var city = $('.user-city-input').val();
-        this.model.fetch({
-            traditional: true,
-            data: $.param({
-                q: city,
-                units: 'imperial',
-                cnt: 10
-            }),
-            success: function() {},
-            error: function(err) {}
-        });
+        if (city === '' || city === undefined || city === null) {
+            $('.user-city-input').css('border', '1px solid red');
+        } else {
+            $('.user-city-input').css('border', 'none');
+            this.model.fetch({
+                traditional: true,
+                data: $.param({
+                    q: city,
+                    units: 'imperial',
+                    cnt: 10
+                }),
+                success: function() {},
+                error: function(err) {}
+            });
+        }
+    }
+});
+
+var DetailedWeatherView = Backbone.View.extend({
+    el: '.weather-holder',
+    events: {
+        "click .fetch-detailed-trigger": "fetchDetailedWeather",
+        "click .fetch-summary-trigger": "fetchSummaryWeather"
     },
     fetchDetailedWeather: function(e) {
-        $('.detailed').show()
+        $('.fetch-summary-trigger').removeClass('active');
+        $('.fetch-detailed-trigger').addClass('active');
+        $('.detailed').show();
+    },
+    fetchSummaryWeather: function(e) {
+        $('.fetch-summary-trigger').addClass('active');
+        $('.fetch-detailed-trigger').removeClass('active');
+        $('.detailed').hide();
     }
 });
 
 var view = new WeatherView();
+var detailedView = new DetailedWeatherView();
